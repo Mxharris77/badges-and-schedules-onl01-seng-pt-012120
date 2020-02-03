89fdd3163fe02611d933f49aa11e1e77c34e7caf
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 02:15:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,8 @@ end
 
 
 def batch_badge_creator(array)
-  array = []
+  greeting_array = []
   array.each do |name|
-    array.push ("Hello, my name is #{name}")
+    greeting_array.push ("Hello, my name is #{name}")
 end
 end
