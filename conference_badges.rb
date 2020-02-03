feb38acf4d8c531dec26ee6770d009dbed39075b
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 02:08:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+
+end
+
+
+
+def batch_badge_creator(attendees)
+ return "Hello, #{attendees}! You'll be assigned to room #{room}!"
+end
+
